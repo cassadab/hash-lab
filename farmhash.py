019ec2564cbbc7115520de29478190d306a37e6b
Brady Cassada <cassadab@msoe.edu>
2019-05-20 23:49:06 -0500

    added method to convert a string to u_int64

--- farmhash.py
+++ farmhash.py
@@ -2,11 +2,6 @@
 import constants
 import ctypes
 
-
-
-def mur(input):
-    print(constants.hi)
-
 def fmix(input):
     input32 = ctypes.c_uint32(input)
     input32.value ^= input >> 16
@@ -17,10 +12,23 @@ def fmix(input):
     hash = hex(input32.value)
     hash = hash[2:]
     hash = hash[:-1]
-    print(hash)
+    print("\nHash: " + hash + "\n")
 
-num_in = 0 
-while(num_in != -1):
-    print("Enter a number to hash")
-    num = int(input())
-    fmix(num)
+# Converts string to u_int64 
+def string_to_int64(string):
+    c_string = ctypes.c_char_p(string)
+    c_int = ctypes.c_uint64()
+    ctypes.memmove(ctypes.addressof(c_int), c_string, ctypes.sizeof(c_int))
+    return c_int
+
+
+# def hash_len0to64(message, length):
+
+
+# num_in = 0 
+# while(num_in != -1):
+#     print("Enter a number to hash")
+#     num = int(input())
+#     fmix(num)
+
+print(string_to_int64("wassup"))
